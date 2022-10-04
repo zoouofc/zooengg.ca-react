bee565b2ef784578494bf759b7aee7772d348195
Poncho <ineswinner@hotmail.com>
2022-10-03 21:25:35 -0600

    Fixing linter issues

--- src/pages/JoinPage.jsx
+++ src/pages/JoinPage.jsx
@@ -6,7 +6,7 @@ import '../stylesheets/JoinPage.scss';
 
 // Importing documents and images
 // Uncomment when in use
-import commissionerApp from '../assets/council-pages/CommissionerForm.pdf';
+// import commissionerApp from '../assets/council-pages/CommissionerForm.pdf';
 // import executiveApp from '../assets/council-pages/ExecutiveOfficerForm.pdf';
 import joinUs from '../assets/council-pages/join-us.svg';
 
