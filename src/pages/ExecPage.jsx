f4aed3c0440415270fa843989d8a18dce31679fd
Poncho <ineswinner@hotmail.com>
2024-07-15 19:56:08 -0600

    President info

--- src/pages/ExecPage.jsx
+++ src/pages/ExecPage.jsx
@@ -34,13 +34,14 @@ const ExecPage = () => (
         email="president@zooengg.ca"
       >
         {/* Bio -- Update for new Exec */}
+        Hello! My name is Yashpreet, and I'm entering my final year of Electrical Engineering. I've been part of ZOO for the better part of my academic journey and have always loved being part of this close-knit community. This year, I'm excited to serve as the president of ZOO, and I look forward to ensuring everyone has the best experience possible. Feel free to message or email me if you have any concerns or just want to say hi!
       </ExecCard>
 
       <ExecCard
         imageSource={academic}
         alt="academic-headshot"
         name="Jun Chao" // Change name for new Exec
-        councilPosition="VP Academic"
+        councilPosition="Co-VP Academic"
         email="academic@zooengg.ca"
       >
         {/* Bio -- Update for new Exec */}
@@ -50,7 +51,7 @@ const ExecPage = () => (
         imageSource={academic}
         alt="academic-headshot"
         name="Simon Xu" // Change name for new Exec
-        councilPosition="VP Academic"
+        councilPosition="Co-VP Academic"
         email="academic@zooengg.ca"
       >
         {/* Bio -- Update for new Exec */}
@@ -60,7 +61,7 @@ const ExecPage = () => (
         imageSource={events}
         alt="events-headshot"
         name="Aaryan Deshpande" // Change name for new Exec
-        councilPosition="VP Events"
+        councilPosition="Co-VP Events"
         email="events@zooengg.ca"
       >
         {/* Bio -- Update for new Exec */}
@@ -70,7 +71,7 @@ const ExecPage = () => (
         imageSource={events}
         alt="events-headshot"
         name="Cody Diao" // Change name for new Exec
-        councilPosition="VP Events"
+        councilPosition="Co-VP Events"
         email="events@zooengg.ca"
       >
         {/* Bio -- Update for new Exec */}
@@ -80,7 +81,7 @@ const ExecPage = () => (
         imageSource={external2}
         alt="external-headshot"
         name="Arshiya Shaheen" // Change name for new Exec
-        councilPosition="VP External"
+        councilPosition="Co-VP External"
         email="external@zooengg.ca"
       >
         {/* Bio -- Update for new Exec */}
@@ -91,7 +92,7 @@ const ExecPage = () => (
         imageSource={external}
         alt="external-headshot"
         name="Muhammad Rafiuzzaman" // Change name for new Exec
-        councilPosition="VP External"
+        councilPosition="Co-VP External"
         email="external@zooengg.ca"
       >
          Hey there! I'm Rafi, and I’m thrilled to take on the role of your Vice President External this year! As I gear up for my third year in electrical engineering, I'm eager to channel my passion and expertise into this exciting opportunity. At ZOO, I'm all about fostering meaningful connections and partnerships with external stakeholders. Whether it's collaborating with industry leaders, seeking out sponsorship opportunities, or representing our organization in the wider community, I'm dedicated to ensuring that our initiatives thrive and make a lasting impact.
@@ -101,7 +102,7 @@ const ExecPage = () => (
         imageSource={finance}
         alt="finance-headshot"
         name="Albert Tri" // Change name for new Exec
-        councilPosition="VP Finance"
+        councilPosition="Co-VP Finance"
         email="finance@zooengg.ca"
       >
         {/* Bio -- Update for new Exec */}
@@ -111,7 +112,7 @@ const ExecPage = () => (
         imageSource={finance}
         alt="finance-headshot"
         name="Tanveer Parhar" // Change name for new Exec
-        councilPosition="VP Finance"
+        councilPosition="Co-VP Finance"
         email="finance@zooengg.ca"
       >
         {/* Bio -- Update for new Exec */}
